64323ad379ef72d872e79435c331779a9d9369a3
Heema Sharma <HeemaS@sfwltd.co.uk>
2018-11-28 18:44:39 +0530

    create training list using xml
    create training list using xml

---
+++ src/webparts/trainingWebpartFlow/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ITrainingWebpartFlowWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'TrainingWebpartFlowWebPartStrings' {
+  const strings: ITrainingWebpartFlowWebPartStrings;
+  export = strings;
+}
