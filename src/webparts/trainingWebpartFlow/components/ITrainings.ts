e25ae219b3932d1f85f6b6e355b8368b896e1115
Hima <36698844+heemas@users.noreply.github.com>
2018-12-02 10:32:50 -0800

    fetching list

---
+++ src/webparts/trainingWebpartFlow/components/ITrainings.ts
@@ -0,0 +1,6 @@
+export interface ISPTrainingsItem{
+    Title: string;
+    TrainingDate: Date;
+    TrainingStatus: string;
+    TrainingApprover: string;
+}
--- src/webparts/trainingWebpartFlow/components/TrainingWebpartFlow.tsx
+++ src/webparts/trainingWebpartFlow/components/TrainingWebpartFlow.tsx
@@ -2,24 +2,62 @@ import * as React from 'react';
 import styles from './TrainingWebpartFlow.module.scss';
 import { ITrainingWebpartFlowProps } from './ITrainingWebpartFlowProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import pnp from 'sp-pnp-js';
+import { ClassTrainings } from './ClassTrainings';
+import { ISPTrainingsItem } from './ITrainings';
 
-export default class TrainingWebpartFlow extends React.Component<ITrainingWebpartFlowProps, {}> {
+export default class TrainingWebpartFlow extends React.Component<ITrainingWebpartFlowProps, any> {
+  public constructor(props:ITrainingWebpartFlowProps,any){
+    super(props);
+    this.state = {
+      items:[]
+      };
+  }
+
   public render(): React.ReactElement<ITrainingWebpartFlowProps> {
     return (
       <div className={ styles.trainingWebpartFlow }>
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
+              <span className={ styles.title }>YaY SharePointers!</span>
+              <p className={ styles.subTitle }>CRUD operations using React + SPFx + PnP Js + Ms Flow.</p>
+              {/* <p className={ styles.description }>{escape(this.props.description)}</p>
               <a href="https://aka.ms/spfx" className={ styles.button }>
                 <span className={ styles.label }>Learn more</span>
-              </a>
+              </a> */}
+              {
+                this.state.items.map(function(item:ISPTrainingsItem){
+                  return(
+                    <div className={"ms-Grid-col ms-sm6 ms-md6 ms-lg4"}>               
+                  <div >
+                  <label className="ms-Label ms-font-xxl">{item.Title}</label>
+                  <label className="ms-Label">{item.TrainingDate}</label>
+                  <label className="ms-Label">{item.TrainingStatus}</label>
+                  <label className="ms-Label">{item.TrainingApprover}</label>                              
+                  </div>
+                  </div>
+                  )
+                })
+              }
             </div>
           </div>
         </div>
       </div>
     );
+  }
+
+  public componentDidMount(){
+    debugger;
+    this._getListTrainings();
+  }
+
+  private _getListTrainings():void{
+    pnp.sp.web.lists.getByTitle(`TrainingList1`).items.get().then
+    ((response) => {
+      let trainingCollection = response.map(item => new ClassTrainings(item));
+      this.setState({items:trainingCollection});
+    }
+    )
   }
 }
